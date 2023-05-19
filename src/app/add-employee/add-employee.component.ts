5d5b1342e290710293269e7bd5ff461c37956338
Bee Jar <BeeJar@rajeebs-mbp-2.lan>
2023-05-19 15:33:17 -0700

    form added to add new employees

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-employee',
@@ -6,7 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-employee.component.css']
 })
 export class AddEmployeeComponent {
-
-
+  
+  profileData = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    dob: new FormControl(''),
+    hireDate: new FormControl(''),
+    phoneNumber: new FormControl('')
+  });
+  
+  onSubmit(){
+    console.warn(this.profileData.value); 
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent,
+    AddEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
